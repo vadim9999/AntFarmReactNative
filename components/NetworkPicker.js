ddd2cd4570dd54d14a63784648f487ce1c8da1de
Vadim <vadim.putrov@gmail.com>
2019-05-18 23:39:04 +0300

    clean up NetworkPicker

--- components/NetworkPicker.js
+++ components/NetworkPicker.js
@@ -7,65 +7,25 @@ export default class NetworkPicker extends React.Component {
         super(props)
         this.state = {
             network: "",
-            // activity: true,
-            // networks: []
-
         }
     }
+
     getListOfNetworks(networks) {
-        console.log("getListOfNetworks");
-
-            if (networks != undefined && networks.length >= 1) {
-                
-                // if (networks.length >= 1 & this.state.network == ""){
-                //     // this.props.getNetwork(networks[0])
-                // }
-            
-            // if(this.state.network === ""){
-            //     this.setState({
-            //         network:networks[0]
-            //     })
-            // }
-                
-                // if (connectedRouter != "") {
-                //     // @TODO not working 
-                //     var index = networks.indexOf(connectedRouter)
-                //     // console.log("index");
-                //     // console.log(index);
-
-
-                //     // if (index != -1) {
-                //     //     var tmp = networks[0]
-                //     //     networks[0] = networks[index] + " підключено"
-                //     //     networks[index] = tmp
-                //     // }
-                // }
-
-
-                return (
-                    networks.map(network => {
-                        if (network != undefined){
-                            
-                                return <Picker.Item key={uuidv1} label={network} value={network} />
-                            
-                            
-                                
-                        }
-
-                        
-                        // else return <Picker.Item label={""} value={""} />
+        if (networks != undefined && networks.length >= 1) {
+            return (
+                networks.map(network => {
+                    if (network != undefined) {
+                        return <Picker.Item key={uuidv1} label={network} value={network} />
                     }
+                }
 
-                    ))
-            }
-        
+                ))
+        }
+
     }
 
     render() {
-        // console.log(this.state.enabled);
-        // {/* enabled= {this.state.enabled} */}
         return (
-
             <Picker
                 enabled={this.props.enabled}
                 selectedValue={this.state.network}
@@ -80,8 +40,6 @@ export default class NetworkPicker extends React.Component {
                 {this.getListOfNetworks(this.props.networks)}
 
             </Picker>
-
-
         )
     }
 }
