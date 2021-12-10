acc78a723966ef6ec46448830d8be3d3317df57d
Vadim <vadim.putrov@gmail.com>
2021-12-10 14:04:47 +0200

    Revert "try to update"
    This reverts commit 5039b00fa4a26c3ba4dd8e1aa03f7f5a18048631.

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import { createAppContainer, createDrawerNavigator } from 'react-navigation';
+import AboutApplication from './screens/aboutApplication/AboutApplication';
+import MainScreen from './screens/main/MainScreen';
+
+const MyDrawerNavigator = createDrawerNavigator(
+  {
+    HomeScreen: {
+      screen: MainScreen,
+    },
+    About: {
+      screen: AboutApplication,
+    },
+
+  },
+);
+
+const App = createAppContainer(MyDrawerNavigator);
+
+export default App;
