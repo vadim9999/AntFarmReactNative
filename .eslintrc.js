c9285c044bc98bb9c74a20a23bf7fb7a79cb89d3
Vadim <vadim.putrov@gmail.com>
2021-12-10 15:45:27 +0200

    react-native upgrade from 0.59.10 to 0.60.0

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,4 @@
 module.exports = {
-  env: {
-    es6: true,
-    node: true,
-  },
-  extends: 'airbnb',
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
-  },
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 2018,
-    sourceType: 'module',
-  },
-  plugins: [
-    'react',
-  ],
-  rules: {
-  },
+  root: true,
+  extends: '@react-native-community',
 };
