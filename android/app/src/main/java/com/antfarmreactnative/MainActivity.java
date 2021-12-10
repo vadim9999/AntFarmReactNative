e1b44511e15ddeceec66ee81b5aa905394d09a36
Vadim <vadim.putrov@gmail.com>
2021-12-10 16:55:26 +0200

    complete upgrading

--- android/app/src/main/java/com/antfarmreactnative/MainActivity.java
+++ android/app/src/main/java/com/antfarmreactnative/MainActivity.java
@@ -7,14 +7,15 @@ import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 
 public class MainActivity extends ReactActivity {
 
-    /**
-     * Returns the name of the main component registered from JavaScript.
-     * This is used to schedule rendering of the component.
-     */
-    @Override
-    protected String getMainComponentName() {
-        return "AntFarmReactNative";
-    }
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "AntFarmReactNative";
+  }
+  
     @Override
  protected ReactActivityDelegate createReactActivityDelegate() {
     return new ReactActivityDelegate(this, getMainComponentName()) {
