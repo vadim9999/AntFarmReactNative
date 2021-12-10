5039b00fa4a26c3ba4dd8e1aa03f7f5a18048631
Vadim <vadim.putrov@gmail.com>
2021-12-10 13:51:45 +0200

    try to update

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+// import 'react-native-gesture-handler';
+import React from "react";
+// import { createAppContainer, createDrawerNavigator } from 'react-navigation';
+// import { NavigationContainer } from '@react-navigation/native';
+// import AboutApplication from './screens/aboutApplication/AboutApplication';
+// import MainScreen from './screens/main/MainScreen';
+
+// const MyDrawerNavigator = createDrawerNavigator({
+//   HomeScreen: {
+//     screen: MainScreen,
+//   },
+//   About: {
+//     screen: AboutApplication,
+//   },
+// });
+
+const App = function () {
+  return <>Hello</>;
+  // return <NavigationContainer>Hello</NavigationContainer>;
+};
+
+export default App;
