970cc3ed7a5904d030346523b6f56474e9d50e2c
Vadim <vadim.putrov@gmail.com>
2021-12-11 11:05:51 +0200

    temporary comment code

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
-import { createAppContainer, createDrawerNavigator } from 'react-navigation';
+import React from 'react';
+import {createAppContainer, createDrawerNavigator} from 'react-navigation';
 import AboutApplication from './screens/aboutApplication/AboutApplication';
 import MainScreen from './screens/main/MainScreen';
+import {Text} from 'react-native';
 
-const MyDrawerNavigator = createDrawerNavigator(
-  {
-    HomeScreen: {
-      screen: MainScreen,
-    },
-    About: {
-      screen: AboutApplication,
-    },
-
+const MyDrawerNavigator = createDrawerNavigator({
+  HomeScreen: {
+    screen: MainScreen,
   },
-);
+  About: {
+    screen: AboutApplication,
+  },
+});
 
-const App = createAppContainer(MyDrawerNavigator);
+// const App = createAppContainer(MyDrawerNavigator);
+
+const App = () => {
+  return <Text>Hello</Text>;
+};
 
 export default App;
