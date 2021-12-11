01f9ebe27ea58983f30c285f0bfd1fc780ca6a69
Vadim <vadim.putrov@gmail.com>
2021-12-11 17:07:01 +0200

    add Native BaseProvider

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import AboutApplication from './screens/aboutApplication/AboutApplication';
 import MainScreen from './screens/main/MainScreen';
+import { NativeBaseProvider, Box, Center } from 'native-base';
 
 // const MyDrawerNavigator = createDrawerNavigator({
 //   HomeScreen: {
@@ -16,8 +17,13 @@ import MainScreen from './screens/main/MainScreen';
 
 const App = () => {
   return (
+    // TODO setting up drawer
     <NavigationContainer>
-      <MainScreen />
+      <NativeBaseProvider>
+        <Center flex={1} px="3">
+          <MainScreen />
+        </Center>
+      </NativeBaseProvider>
     </NavigationContainer>
   );
 };
