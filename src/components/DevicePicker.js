04dca07b3d2a2e23dea75fa181de828d7853340f
Vadim <vadim.putrov@gmail.com>
2021-12-11 17:08:20 +0200

    migration on react navigation 6

--- src/components/DevicePicker.js
+++ src/components/DevicePicker.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Picker } from 'native-base';
+import { Select, CheckIcon } from 'native-base';
 import uuidv1 from 'uuid/v1';
 import styles from './styles';
 
@@ -11,37 +11,78 @@ export default class DevicePicker extends React.Component {
     };
   }
 
-  getListOfDevices(devices) {
-    if (devices != undefined && devices.length > 0) {
-      return (
-        devices.map((device) => {
-          let pair = '';
-          if (device.uuids !== undefined && device.uuids.length > 0) {
-            pair = 'Пара';
-          }
-          if (device !== undefined && device.name != undefined) return <Picker.Item key={uuidv1()} label={`${device.name} ${device.address} ${pair}`} value={device.address} />;
-          if (device !== undefined && device.address != undefined) {
-            return <Picker.Item key={uuidv1()} label={`${'NoName '}${device.address}`} value={device.address} />;
-          } return <Picker.Item key={uuidv1()} label="" value="" />;
-        }));
-    }
-  }
+  // TODO mayber it can be removed
+  // getListOfDevices(devices) {
+  //   if (devices?.length) {
+  //     const deviceList = devices.map((device) => {
+  //       let pair = '';
+  //       if (device.uuids.length) {
+  //         pair = 'Пара';
+  //       }
+  //       if (device !== undefined && device.name != undefined)
+  //         return (
+  //           <Picker.Item
+  //             key={uuidv1()}
+  //             label={`${device.name} ${device.address} ${pair}`}
+  //             value={device.address}
+  //           />
+  //         );
+  //       if (device !== undefined && device.address != undefined) {
+  //         return (
+  //           <Picker.Item
+  //             key={uuidv1()}
+  //             label={`${'NoName '}${device.address}`}
+  //             value={device.address}
+  //           />
+  //         );
+  //       }
+  //       return <Picker.Item key={uuidv1()} label="" value="" />;
+  //     });
+
+  //     return deviceList;
+  //   }
+  // }
 
   render() {
     const { devices } = this.props;
     return (
-      <Picker
+      <Select
+        minWidth="200"
+        accessibilityLabel="Виберіть ферму"
+        placeholder="Виберіть ферму"
         selectedValue={this.state.device}
-        
-        onValueChange={(itemValue, itemIndex) => {
+        onValueChange={itemValue => {
           this.setState({ device: itemValue });
           this.props.getItem(itemValue);
         }}
-      >
-        {this.getListOfDevices(devices)}
+        _selectedItem={{
+          bg: 'teal.600',
+          endIcon: <CheckIcon size={5} />,
+        }}
+        mt="1">
+        {devices.length
+          ? devices.map.map(device => {
+              const hasPair = device.uuids.length ? 'Пара' : '';
 
-      </Picker>
-
+              return (
+                <Select.Item
+                  label={`${device.name ?? 'No name'} ${
+                    device.address
+                  } ${hasPair}`}
+                  value="ux"
+                />
+              );
+            })
+          : []}
+      </Select>
+      // <Picker
+      //   selectedValue={this.state.device}
+      //   onValueChange={(itemValue, itemIndex) => {
+      //     this.setState({device: itemValue});
+      //     this.props.getItem(itemValue);
+      //   }}>
+      //   {this.getListOfDevices(devices)}
+      // </Picker>
     );
   }
 }
