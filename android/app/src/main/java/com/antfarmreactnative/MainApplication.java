be875a59b9815514a20f0183f9193e1da9e3bcfd
Vadim <vadim.putrov@gmail.com>
2021-12-17 19:57:02 +0200

    install drawer and icons lib

--- android/app/src/main/java/com/antfarmreactnative/MainApplication.java
+++ android/app/src/main/java/com/antfarmreactnative/MainApplication.java
@@ -11,6 +11,10 @@ import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
+// Needs for Reanimated v2
+import com.facebook.react.bridge.JSIModulePackage;
+import com.swmansion.reanimated.ReanimatedJSIModulePackage;
+
 public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost =
@@ -32,6 +36,12 @@ public class MainApplication extends Application implements ReactApplication {
         @Override
         protected String getJSMainModuleName() {
           return "index";
+        }
+
+        // Needs for Reanimated v2
+        @Override      
+        protected JSIModulePackage getJSIModulePackage() {
+          return new ReanimatedJSIModulePackage();
         }
       };
 
