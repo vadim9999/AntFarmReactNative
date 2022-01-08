7c6b5c39064c98d8a653af7ad7f9f9e295c60144
Vadim <vadim.putrov@gmail.com>
2022-01-08 09:36:47 +0200

    add isBluetoothAvailable to redux store

--- src/redux/slices/deviceSlice.ts
+++ src/redux/slices/deviceSlice.ts
@@ -2,10 +2,12 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
 interface DeviceState {
   deviceAddress: string | null;
+  isBluetoothAvailable: boolean;
 }
 
 const initialState: DeviceState = {
   deviceAddress: null,
+  isBluetoothAvailable: false,
 };
 
 export const deviceSlice = createSlice({
@@ -15,9 +17,13 @@ export const deviceSlice = createSlice({
     setDeviceAddress: (state, action: PayloadAction<string | null>) => {
       state.deviceAddress = action.payload;
     },
+    setIsBluetoothAvailable: (state, action: PayloadAction<boolean>) => {
+      state.isBluetoothAvailable = action.payload;
+    },
   },
 });
 
-export const { setDeviceAddress } = deviceSlice.actions;
+export const { setDeviceAddress, setIsBluetoothAvailable } =
+  deviceSlice.actions;
 
 export default deviceSlice.reducer;
