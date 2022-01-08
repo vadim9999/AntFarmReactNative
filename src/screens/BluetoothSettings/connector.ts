dacbd6b3b5bdb1b0aa0753fc01ee0e6ba3b3dd43
Vadim <vadim.putrov@gmail.com>
2022-01-08 12:14:31 +0200

    add setDeviceAddress to the BluetoothSettings

--- src/screens/BluetoothSettings/connector.ts
+++ src/screens/BluetoothSettings/connector.ts
@@ -1,9 +1,14 @@
 import { connect, ConnectedProps } from 'react-redux';
 import { RootState } from 'redux/store/store';
+import { setDeviceAddress } from 'redux/slices/deviceSlice';
 
 const mapStateToProps = (state: RootState) => ({
   isBluetoothAvailable: state.device.isBluetoothAvailable,
 });
 
-export const connector = connect(mapStateToProps);
+const mapDispathcToProps = {
+  setDeviceAddress,
+};
+
+export const connector = connect(mapStateToProps, mapDispathcToProps);
 export type PropsFromRedux = ConnectedProps<typeof connector>;
