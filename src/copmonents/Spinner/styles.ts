b4cca33af6a11d58a8c966c3b00cdfcc7b153eab
Vadim <vadim.putrov@gmail.com>
2022-01-29 13:07:47 +0200

    add background to spinner

--- src/copmonents/Spinner/styles.ts
+++ src/copmonents/Spinner/styles.ts
@@ -3,6 +3,7 @@ import { StyleSheet } from 'react-native';
 const styles = StyleSheet.create({
   spinnerContainer: {
     position: 'absolute',
+    backgroundColor: '#0000004a',
     zIndex: 2,
     width: '100%',
     height: '100%',
